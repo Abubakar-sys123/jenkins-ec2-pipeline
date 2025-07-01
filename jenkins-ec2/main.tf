34c3c946ef97287e169b06a3de0c4d76d29b0065
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:38:33 +0500

    Update main.tf

--- jenkins-ec2/main.tf
+++ jenkins-ec2/main.tf
@@ -1,1 +1,14 @@
-
+module "nat_gateway" {
+  source                   = "./nat-resource"
+  public_subnet_id         = module.vpc.public_subnet_id
+  project = var.project
+}
+module "vpc" {
+  source               = "./vpc-resource"
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidr   = var.public_subnet_cidr
+  public_subnet_cidr_1 = var.public_subnet_cidr_1
+  private_subnet_cidr  = var.private_subnet_cidr
+  availability_zone    = var.availability_zone
+  nat_gateway_id       = module.nat_gateway.nat_gateway_id
+}
