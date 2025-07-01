1d6b4c936dac4a61f452b8c502dfcd929d183fab
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:37:46 +0500

    Update backend.tf

--- jenkins-ec2/backend.tf
+++ jenkins-ec2/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "jenkins-ec2-backend-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "jenkins-ec2-table"
+    encrypt        = true
+  }
+}
 
