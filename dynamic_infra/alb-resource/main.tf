9507ebf285fe0c5c49d70fed2e016ced7a1dc217
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:29:32 +0500

    Update main.tf

--- dynamic_infra/alb-resource/main.tf
+++ dynamic_infra/alb-resource/main.tf
@@ -1,1 +1,46 @@
 
+resource "aws_lb" "app_alb" {
+  name               = "${var.project}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
+  subnets            = var.public_subnet_ids
+
+  tags = {
+    Name = "${var.project}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "alb_tg" {
+  name     = "${var.project}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "target_attachment" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = var.private_instance_id
+  port             = 80
+}
