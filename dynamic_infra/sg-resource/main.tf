0470d054a9a1e9f64eace814af817b47fbf7ecd4
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:32:22 +0500

    Update main.tf

--- dynamic_infra/sg-resource/main.tf
+++ dynamic_infra/sg-resource/main.tf
@@ -1,1 +1,77 @@
 
+
+# resource "aws_security_group" "web_sg" {
+#   name        = var.sg_name
+#   description = "Allow HTTP and SSH"
+#   vpc_id      = var.aws_vpc
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = [var.ingress_cidr]
+#   }
+
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = [var.ingress_cidr]
+#   }
+#     ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = [var.ingress_cidr]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+resource "aws_security_group" "private_web_sg" {
+  name        = var.private_web_sg
+  description = "Allow HTTP from ALB only"
+  vpc_id      = var.aws_vpc
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "alb_sg" {
+  name        = var.alb_sg_name
+  description = "Allow HTTP inbound traffic for ALB"
+  vpc_id      = var.aws_vpc
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
