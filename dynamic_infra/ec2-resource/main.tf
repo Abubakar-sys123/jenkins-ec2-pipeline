c01cb848ca2b74b16c618e569464cfb21b6c785f
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:31:06 +0500

    Update main.tf

--- dynamic_infra/ec2-resource/main.tf
+++ dynamic_infra/ec2-resource/main.tf
@@ -1,1 +1,42 @@
+resource "aws_instance" "private_web_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.private_subnet_id
+  vpc_security_group_ids = [var.private_web_sg]
+  key_name               = var.key_name
+  associate_public_ip_address = false
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
 
+  user_data = var.user_data
+
+  tags = {
+    Name = "Private-EC2-${var.private_ip}"
+  }
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name = "ec2_ssm_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ec2_ssm_instance_profile"
+  role = aws_iam_role.ssm_role.name
+}
