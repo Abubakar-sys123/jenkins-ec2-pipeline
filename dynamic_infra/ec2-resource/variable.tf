fbd88836540b82d0bd75e852215de477b6f35341
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:31:56 +0500

    Update variable.tf

--- dynamic_infra/ec2-resource/variable.tf
+++ dynamic_infra/ec2-resource/variable.tf
@@ -1,1 +1,23 @@
+variable "ami_id" {}
+# variable "instance_type" {}
+variable "key_name" {}
+variable "private_subnet_id" {}
+variable "private_web_sg" {}
+variable "aws_region" {
+  type = string
+}
+variable "ami_type" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
 
+variable "user_data" {
+  description = "User data script based on ami_type"
+  type        = string
+}
+variable "private_ip" {
+  type        = string
+  description = "Static private IP for EC2"
+}
