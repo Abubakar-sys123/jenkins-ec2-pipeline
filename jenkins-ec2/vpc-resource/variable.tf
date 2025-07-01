6bdd02e505fe8633bcb1534310e3b0e93629d280
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:37:21 +0500

    Update variable.tf

--- jenkins-ec2/vpc-resource/variable.tf
+++ jenkins-ec2/vpc-resource/variable.tf
@@ -1,1 +1,26 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
 
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+variable "public_subnet_cidr_1" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+variable "availability_zone" {
+  description = "Availability Zone for the subnet"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+variable "nat_gateway_id" {
+  description = "NAT Gateway ID for private subnet internet access"
+  type        = string
+}
