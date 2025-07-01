7b3f994ac7eaefd7aee18dcb3706e623396e98cd
Muhammad Abubakar <muhammad.abubakar0633@gmail.com>
2025-07-01 20:31:37 +0500

    Update output.tf

--- dynamic_infra/ec2-resource/output.tf
+++ dynamic_infra/ec2-resource/output.tf
@@ -1,1 +1,16 @@
+output "private_ec2_instance_id" {
+  description = "ID of the private EC2 instance"
+  value       = aws_instance.private_web_server.id
+}
+
+output "private_ec2_private_ip" {
+  description = "Private IP address of the private EC2 instance"
+  value       = aws_instance.private_web_server.private_ip
+}
+
+output "private_ec2_availability_zone" {
+  description = "Availability zone of the private EC2 instance"
+  value       = aws_instance.private_web_server.availability_zone
+}
+
 
